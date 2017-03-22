dd5699e036454f8123dab55b518400ec9b40e5f8
Ann Siapno <ann.n.siapno@gmail.com>
2017-03-22 16:57:40 +1100

    Form functional
    Form functional

---
+++ form.php
@@ -0,0 +1,25 @@
+<?php
+if(empty($_POST['submit']))
+{
+
+echo "Form is not submitted!";
+exit;
+
+}
+
+if(empty($_POST["fullname"]) ||
+empty($_POST["email"]))
+{
+
+echo "Please fill the form";
+exit;
+
+}
+
+$name = $_POST["fullname"];
+$email = $_POST["email"];
+
+mail( 'ann.n.siapno@gmail.com' , 'New form submission' ,
+"New form submission: Name: $name, Email:$email"  );
+// header('Location: thank-you.html');
+?>
