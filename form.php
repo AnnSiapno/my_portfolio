3743dbf3dcacab7679074481eaf22a9e7d394d8f
Ann Siapno <ann.n.siapno@gmail.com>
2017-03-22 17:06:25 +1100

    Form update
    Form update

--- form.php
+++ form.php
@@ -9,7 +9,8 @@ exit;
 
 if(empty($_POST["name"]) ||
 empty($_POST["number"])) ||
-empty($_POST["email"]))
+empty($_POST["email"])) ||
+empty($_POST["message"]))
 {
 
 echo "Please fill the form";
@@ -20,8 +21,9 @@ exit;
 $name = $_POST["name"];
 $number = $_POST["number"];
 $email = $_POST["email"];
+$message = $_POST["message"];
 
 mail( 'ann.n.siapno@gmail.com' , 'New form submission' ,
-"New form submission: Name: $name, Number: $number, Email:$email"  );
+"New form submission: Name: $name, Number: $number, Email: $email, Message: $message"  );
 // header('Location: thank-you.html');
 ?>
